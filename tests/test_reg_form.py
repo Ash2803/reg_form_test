c8c9d15f6f8f64f66e6165365e5f8093e8dc24d1
Ashot Abazyan <ashot.93@mail.ru>
2022-06-26 17:04:59 +0300

    26/06

--- tests/test_reg_form.py
+++ tests/test_reg_form.py
@@ -15,6 +15,13 @@ def test_fill_form():
     browser.element('[value="1993"]').click()
     browser.element('[value="2"]').click()
     browser.element('div[aria-label="Choose Sunday, March 28th, 1993"]').click()
-
-
-
+    browser.element('#subjectsInput').type("Math").press_tab()
+    browser.element('#subjectsInput').type("History").press_tab()
+    browser.element("[for='hobbies-checkbox-1']").click()
+    browser.element("[for='hobbies-checkbox-3']").click()
+    browser.element('#uploadPicture').type("C:/Users/Ash/PycharmProjects/selene_test_box/159627.png")
+    browser.element("#currentAddress").type("Moscow")
+    browser.element("#react-select-3-input").type("NCR").press_tab()
+    browser.element("#react-select-4-input").type("Delhi").press_tab()
+    browser.execute_script("document.querySelector('#app > footer').style.display='none'")
+    browser.element("#submit").click()

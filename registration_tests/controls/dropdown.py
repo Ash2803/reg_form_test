1c102053a1645d5d9c1b2152cd7ae924a0c0acf0
Ashot Abazyan <ashot.93@mail.ru>
2022-07-04 23:31:36 +0300

    added controls

--- registration_tests/controls/dropdown.py
+++ registration_tests/controls/dropdown.py
@@ -3,10 +3,10 @@ from selene.core.entity import SeleneElement
 from selene.support.shared import browser
 
 
-def select(element: SeleneElement, /, *, option: str):
+def by_choose(element: SeleneElement, /, *, option: str):
     element.perform(command.js.scroll_into_view).click()
     browser.all('[id^=react-select-][id*=-option]').element_by(have.exact_text(option)).click()
 
 
 def select_by(selector: str, /, *, option: str):
-    select(browser.element(selector), option=option)
+    by_choose(browser.element(selector), option=option)

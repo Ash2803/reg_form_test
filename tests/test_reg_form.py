2bff4f39dc01ff361e9d2c0ddf3ae50eaa6f4347
Ashot Abazyan <ashot.93@mail.ru>
2022-07-09 01:19:26 +0300

    changed test structure

--- tests/test_reg_form.py
+++ tests/test_reg_form.py
@@ -4,34 +4,31 @@ from selene import have, command
 from selene.support.shared import browser
 
 
-class Gender:
-    male = '[for="gender-radio-1"]'
-    female = '[for="gender-radio-2"]'
-    other = '[for="gender-radio-3"]'
-
-
-class Hobbies:
-    sports = 'Sports'
-    reading = 'Reading'
-    music = 'Music'
-
-
-class Subjects:
-    physics = 'Physics'
-    english = 'English'
-
-
-class City:
-    state = 'NCR'
-    city = 'Delhi'
-
-
 def student_reg_form_opened():
     browser.open('/automation-practice-form')
 
 
 def test_fill_reg_form():
     student_reg_form_opened()
+
+    class Gender:
+        male = '[for="gender-radio-1"]'
+        female = '[for="gender-radio-2"]'
+        other = '[for="gender-radio-3"]'
+
+    class Hobbies:
+        sports = 'Sports'
+        reading = 'Reading'
+        music = 'Music'
+
+    class Subjects:
+        physics = 'Physics'
+        english = 'English'
+
+    class City:
+        state = 'NCR'
+        city = 'Delhi'
+
     # Act
     browser.element("#firstName").type('Name')
     browser.element("#lastName").type('LastName')
@@ -47,8 +44,12 @@ def test_fill_reg_form():
     browser.element('[value="2"]').click()
     browser.element('div[aria-label="Choose Sunday, March 28th, 1993"]').click()
 
-    browser.element('#subjectsInput').type(Subjects.physics).press_enter()
-    browser.element('#subjectsInput').type(Subjects.english).press_enter()
+    def autocomplete(selector: str, /, *, from_: str):
+        browser.element(selector).type(from_).press_enter()
+        browser.all('.subjects-auto-complete__option').element_by(have.exact_text(from_))
+
+    autocomplete('#subjectsInput', from_=Subjects.physics)
+    autocomplete('#subjectsInput', from_=Subjects.english)
 
     browser.all('.custom-checkbox').element_by(have.exact_text(Hobbies.music)).click()
     browser.all('.custom-checkbox').element_by(have.exact_text(Hobbies.sports)).click()
@@ -70,7 +71,7 @@ def test_fill_reg_form():
     browser.elements("table tr").element(3).should(have.text("Male"))
     browser.elements("table tr").element(4).should(have.text("8999555336"))
     browser.elements("table tr").element(5).should(have.text("28 March,1993"))
-    browser.elements("table tr").element(6).should(have.text(f'{Subjects.physics}, {Subjects.english}'))
+    browser.elements("table tr").element(6).should(have.text('Physics, English'))
     browser.elements("table tr").element(7).should(have.text("Music, Sports, Reading"))
     browser.elements("table tr").element(8).should(have.text("159627.png"))
     browser.elements("table tr").element(9).should(have.text("Moscow"))
